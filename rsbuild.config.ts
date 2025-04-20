b70c3b38d37411aaa7412f298c6471a0743d4ae0
yejihui <1598036836@qq.com>
2025-04-20 18:59:31 +0800

    refactor: 增加源码路径覆盖和调试跟踪配置

--- rsbuild.config.ts
+++ rsbuild.config.ts
@@ -45,10 +45,38 @@ export default defineConfig({
   // },
   tools: {
     // 如果需要对 rspack 进行更细致的配置 (通常不需要)
-    // rspack: (config, { env, HtmlPlugin }) => {
-    //   config.target = 'electron-renderer'; // 明确目标环境
-    //   return config;
-    // },
+    rspack: (config, { env, HtmlPlugin }) => {
+      // config.target = 'electron-renderer'; // 明确目标环境
+      if (process.env.NODE_ENV === 'development') {
+        config.output = config.output ? {
+          ...config.output,
+        } : {};
+        config.output.devtoolFallbackModuleFilenameTemplate = 'webpack:///[resource-path]?[hash]';
+        config.output.devtoolModuleFilenameTemplate = info => {
+          const isVue = info.resourcePath.match(/\.vue$/);
+          const isScript = info.query.match(/type=script/);
+          const hasModuleId = info.moduleId !== '';
+
+          // Detect generated files, filter as webpack-generated
+          if (
+            // Must result from vue-loader
+            isVue
+            // Must not be 'script' files (enough for chrome), or must have moduleId (firefox)
+            && (!isScript || hasModuleId)
+          ) {
+            let pathParts = info.resourcePath.split('/');
+            const baseName = pathParts[pathParts.length - 1];
+            // prepend 'generated-' to filename as well, so it's easier to find desired files via Ctrl+P
+            pathParts.splice(-1, 1, `generated-${baseName}`);
+            return `webpack-generated:///${pathParts.join('/')}?${info.hash}`;
+          }
+
+          // If not generated, filter as webpack-vue
+          return `webpack-vue:///${info.resourcePath}`;
+        }
+      }
+      return config;
+    },
   },
   // 生产环境特定配置
   performance: {
